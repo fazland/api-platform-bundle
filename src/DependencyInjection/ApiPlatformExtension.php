82f80b1730419455f0bc593687ce637a506b1798
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2017-11-21 10:13:30 +0100

    register DecoderInterface for autoconfiguration

--- src/DependencyInjection/ApiPlatformExtension.php
+++ src/DependencyInjection/ApiPlatformExtension.php
@@ -2,6 +2,7 @@
 
 namespace Kcs\ApiPlatformBundle\DependencyInjection;
 
+use Kcs\ApiPlatformBundle\Decoder\DecoderInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -12,8 +13,14 @@ class ApiPlatformExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
         $loader->load('decoders.xml');
         $loader->load('patch_manager.xml');
         $loader->load('view.xml');
+
+        if (method_exists($container, 'registerForAutoconfiguration')) {
+            $container->registerForAutoconfiguration(DecoderInterface::class)
+                ->addTag('kcs_api.decoder');
+        }
     }
 }
