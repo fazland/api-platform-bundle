de37e2294ec8ae1023d71c36bafc44c625532614
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2017-11-22 11:17:42 +0100

    add missed compiler pass registration

--- src/ApiPlatformBundle.php
+++ src/ApiPlatformBundle.php
@@ -2,8 +2,15 @@
 
 namespace Kcs\ApiPlatformBundle;
 
+use Kcs\ApiPlatformBundle\DependencyInjection\CompilerPass\RegisterDecoders;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApiPlatformBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container
+            ->addCompilerPass(new RegisterDecoders());
+    }
 }
