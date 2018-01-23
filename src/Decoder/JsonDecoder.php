f213f1e9b5a2b1943cb1251e784bac068a3c16ec
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2018-01-23 17:10:05 +0100

    throw a bad request exception if json is not valid

--- src/Decoder/JsonDecoder.php
+++ src/Decoder/JsonDecoder.php
@@ -2,6 +2,8 @@
 
 namespace Fazland\ApiPlatformBundle\Decoder;
 
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
 class JsonDecoder implements DecoderInterface
 {
     /**
@@ -13,7 +15,11 @@ class JsonDecoder implements DecoderInterface
             return [];
         }
 
-        $content = @json_decode($content, true);
+        try {
+            $content = self::doDecode($content);
+        } catch (\Exception $e) {
+            throw new BadRequestHttpException('Invalid request body', $e);
+        }
 
         array_walk_recursive($content, function (&$value) {
             if (false === $value) {
@@ -32,5 +38,25 @@ class JsonDecoder implements DecoderInterface
     public static function getFormat(): string
     {
         return 'json';
+    }
+
+    /**
+     * Decodes a json string. Throw an exception if json is not valid.
+     *
+     * @param string $json
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    private static function doDecode(string $json): array
+    {
+        $returnValue = @\json_decode($json, true);
+
+        if (null === $returnValue && JSON_ERROR_NONE !== \json_last_error()) {
+            throw new \Exception('Cannot decode JSON: '.\json_last_error_msg());
+        }
+
+        return $returnValue;
     }
 }
