96f1224d7b5b5962a6599d9f797c7fbe9d01c6b4
Alessandro Chitolina <alekitto@gmail.com>
2019-12-08 03:57:52 +0100

    add support for chronos 2

--- src/Form/DataTransformer/DateTransformer.php
+++ src/Form/DataTransformer/DateTransformer.php
@@ -46,11 +46,11 @@ class DateTransformer implements DataTransformerInterface
 
         $dateTime = Chronos::now()->startOfDay();
         if (\preg_match('/(\d{2})\/(\d{2})\/(\d{4,})/', $value, $matches)) {
-            return $dateTime->setDate($matches[3], $matches[2], $matches[1]);
+            return $dateTime->setDate((int) $matches[3], (int) $matches[2], (int) $matches[1]);
         }
 
         if (\preg_match('/(\d{4,})-(\d{2})-(\d{2})/', $value, $matches)) {
-            return $dateTime->setDate($matches[1], $matches[2], $matches[3]);
+            return $dateTime->setDate((int) $matches[1], (int) $matches[2], (int) $matches[3]);
         }
 
         throw new TransformationFailedException('Unexpected date format');
